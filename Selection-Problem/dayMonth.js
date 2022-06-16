6b03d03e3c6fa963be9da7ad401a10a2b69be24d
praju-nikam <sawanprajakta84@gmail.com>
2022-06-16 14:23:58 +0530

    Added : Day Month between march 20 to june 20

---
+++ Selection-Problem/dayMonth.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+
+var day = prompt("Enter day: ");
+var month = prompt("Enter  Month : ");
+
+if ((day <= 20 && month <= 6) && (day >= 20 && month >= 3) && (day <= 31)) {
+    console.log("True");
+} else {
+    console.log("false");
+}
