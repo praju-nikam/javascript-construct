6e1047be64aa1e475ba1ddc87523fca02e7d015f
praju-nikam <sawanprajakta84@gmail.com>
2022-06-16 14:34:23 +0530

    Added : single digit number and write the number in word using Case

---
+++ Case-Statement/numberInWords.js
@@ -0,0 +1,36 @@
+// Read a single digit number and write the number in word using Case
+let number = Math.floor(Math.random() * 10); // print randomly 0 to 9
+
+switch (number) {
+    case 0:
+        console.log("Number " + number + " = Zero ");
+        break;
+    case 1:
+        console.log("Number " + number + " = One ");
+        break;
+    case 2:
+        console.log("Number " + number + " = Two");
+        break;
+    case 3:
+        console.log("Number " + number + " = Three");
+        break;
+    case 4:
+        console.log("Number " + number + " = Four");
+        break;
+    case 5:
+        console.log("Number " + number + " = Five");
+        break;
+    case 6:
+        console.log("Number " + number + " = Six");
+        break;
+    case 7:
+        console.log("Number " + number + " = Seven");
+        break;
+    case 8:
+        console.log("Number " + number + " = Eight");
+        break;
+    case 9:
+        console.log("Number " + number + " = Nine");
+        break;
+
+}
